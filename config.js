6b671e2adb8af6bc69e831e05499e65288af7038
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-09-23 20:46:15 +0530

    Done Route level middleware

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 const { app, express }  = require('./server/server');
+const router = express.Router();
 
 const path = require('path');
 const publicPath = path.join(__dirname, 'public');
 
+
+
 app.use(express.static(publicPath));
 app.set('view engine', 'ejs');
 
-module.exports = { app, publicPath };
+module.exports = { app, publicPath, router };
--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,29 @@
-const { app, publicPath } = require('../config');
+const { app, router } = require('../config');
 const middleware = require('../middleware/middleware');
 
-// To apply througout all the routes
-// app.use(middleware);
+
+/* 
+ * To apply througout all the routes in the application
+ *
+ * app.use(middleware);
+*/
+
+router.use(middleware);
 
 app.get('/', (_, resp) => {
     resp.render('home');
 });
 
-app.get('/user', middleware, (req, resp) => {
+router.get('/user', (req, resp) => {
     const { age } = req.query;
     resp.render('user', { age });
 });
 
-app.get("*", (_, resp) => {
-    resp.sendFile(`${publicPath}/404.html`);
+router.get('/profile', (req, resp) => {
+    const { age } = req.query;
+    resp.render('user-profile', { age });
 });
+
+app.use('/', router);
 
 module.exports = app;
