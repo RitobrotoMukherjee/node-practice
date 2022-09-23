f04df594df03192c6726d9b12b9c106d2661cbbb
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-09-23 14:22:56 +0530

    Style footer and add layout based architecture

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const server = require('./server/server');
-require('./routes/route')
+const app = require('./routes/route');
 
-server.listen(3030);
+app.listen(3030);
 console.log("Server started on: http://localhost:3030");
--- routes/route.js
+++ routes/route.js
@@ -1,5 +1,12 @@
 const { app, publicPath } = require('../config');
 
+
+app.get('/', (_, resp) => {
+    resp.render('home', { publicPath, title: 'Home Page' })
+});
+
 app.get('*', (_, resp) => {
     resp.sendFile(`${publicPath}/404.html`);
 });
+
+module.exports = app;
