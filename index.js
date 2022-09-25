980fd59cf8274a5d65cf1356e19a2b79f760e98b
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-09-25 20:37:36 +0530

    Done the solution with promises and async/await both

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const mongoD = require('./mongodb');
 
+// mongoD().then((connection) => {
+//     connection.find().toArray().then(data => {
+//         console.log(data);
+//     })
+// })
+
 (async () => {
     const dbConnection = await mongoD();
     const data = await dbConnection.find().toArray();
